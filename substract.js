69573cbdd7086dec22a45c897362355334724207
Simon Dieny <simon.dieny@gmail.com>
2021-01-25 09:43:29 +0100

    handle error for substract method

--- substract.js
+++ substract.js
@@ -1,6 +1,8 @@
 module.exports = function substract(a, b) {
-  if(a < b) {
-    return 0
+  const res = a - b
+  if(res == NaN) {
+    throw new Error('Invalid Operation')
   }
-  return a - b
+  
+  return res
 }
